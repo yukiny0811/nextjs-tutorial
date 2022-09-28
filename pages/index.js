0a956e66969fec7b0cc07fe044ea914a0231ecf2
yukiny0811 <yukiny0811@gmail.com>
2022-09-29 03:12:14 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({allPostsData}) {
       <section className={utilStyles.headingMd}>
         <p>Hello! I'm Yuki!</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
